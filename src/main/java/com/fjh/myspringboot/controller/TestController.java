0ba421e1d531a2bdd3f05bb530a21d8297d11262
房俊含 <853226786@qq.com>
2020-12-17 17:20:10 +0800

    cookie和session

--- src/main/java/com/fjh/myspringboot/controller/TestController.java
+++ src/main/java/com/fjh/myspringboot/controller/TestController.java
@@ -7,10 +7,13 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
 
 
 @RestController
@@ -32,5 +35,38 @@ public class TestController {
 
         TestDto testDto = testService.list(zifu, shuzi);
         return Response.success(testDto);
+    }
+
+    @GetMapping("/putcookie")
+    @ApiOperation("cookie和session")
+    Response putcookie(HttpServletResponse resp, HttpServletRequest rsq,
+                       HttpSession session){
+        resp.addCookie(new Cookie("cookie0", "ab"));
+        resp.addCookie(new Cookie("cookie1", "ac"));
+        session.setAttribute("session1","s1");
+        session.setAttribute("session2","s2");
+
+        Cookie[] cookie = rsq.getCookies();
+        String a0 = null; String a1 = null; String a2 = null;
+        if(cookie != null && cookie.length > 0){
+            a0 = cookie[0].getValue();
+            a1 = cookie[1].getValue(); // 在不清理缓存、 生命周期活跃的情况下, 可以获取到.
+            a2 = cookie[2].getValue(); // 同时, session 也属于 按序设置的 cookie
+            // cookie会报空指针
+        }
+        
+        String session1 = (String) session.getAttribute("session1");
+        String session2 = (String) session.getAttribute("session2");
+        String session3 = (String) session.getAttribute("session3"); // session HttpSession访问即创建
+
+        System.out.println("cookie数组= " + Arrays.toString(cookie));
+        System.out.println("cookie0= " + a0);
+        System.out.println("cookie1= " + a1);
+        System.out.println("cookie2= " + a2);
+        System.out.println("session1= " + session1);
+        System.out.println("session2= " + session2);
+        System.out.println("session3= " + session3);
+
+        return Response.success();
     }
 }
